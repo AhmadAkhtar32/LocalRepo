9f5fdc300664879f0c12c453eb19c1be57ee78a5
AhmadAkhtar32 <ahmadrao3226@gmail.com>
2024-01-21 20:12:49 +0500

    Just need to change

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello Computer Scientist !!!");
+        System.out.println("You are Learning Java here !");
     }
 }
