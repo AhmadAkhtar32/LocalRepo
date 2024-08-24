8cde854d0e2d19d28d350227d066343c0136d2f6
Ahmad Akhtar <147875473+AhmadAkhtar32@users.noreply.github.com>
2024-08-24 09:24:10 +0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,4 +1,4 @@
-/ demo java program/ 
+/ demo java program for practice purpose/ 
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello Computer Scientist !!!");
